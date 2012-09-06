7f04490f91c5646a402ac1197e49a897ee88d2f8
Saif Al-Naib <salnaib@ryerson.ca>
2012-09-06 19:48:47 -0400

    Bundle installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plantourage::Application.config.secret_token = '5ac1d6eef64bcd611bda2963df87ddabc5adbf91dea7970f52dd8cf404264f60bfc107715be1deb47413ccc7770854ad264ff13aed15ab12e3918a69e8b06252'
