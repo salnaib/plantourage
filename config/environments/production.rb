e9fce8883091c8521b396e1f95df63f4a3fceb75
Saif Al-Naib <salnaib@ryerson.ca>
2013-02-19 09:59:18 -0500

    assets precompile in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Plantourage::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( styles.css header.css )
+  config.assets.precompile += %w( custom/styles.css custom/header.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
