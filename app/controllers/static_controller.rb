98de3c3349e8505009cec23e29362c1fbf4380e0
Saif Al-Naib <salnaib@ryerson.ca>
2013-02-19 22:26:53 -0500

    heroku koala GraphMethodException error fix trial 4

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -48,13 +48,12 @@ class StaticController < ApplicationController
   def landing
 
     # Get base API Connection
-    @graph  = Koala::Facebook::API.new()
+    @graph  = Koala::Facebook::API.new(session[:access_token])
 
     # Get public details of current application
     @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
 
     if session[:access_token]
-      @graph  = Koala::Facebook::API.new(session[:access_token])
       @user    = @graph.get_object("me")
       #@friends = @graph.get_connections('me', 'friends')
       #@photos  = @graph.get_connections('me', 'photos')
