7e3d6aa4baf98f2fa4fe744490efec8210738271
Saif Al-Naib <salnaib@ryerson.ca>
2013-02-20 23:36:56 -0500

    Add venue data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130220003458) do
+ActiveRecord::Schema.define(:version => 20130221040322) do
 
   create_table "invites", :force => true do |t|
     t.datetime "created_at", :null => false
